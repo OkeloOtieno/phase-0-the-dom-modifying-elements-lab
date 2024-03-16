5126021eb4e703122b45384dd88c235141ba28cf
Emmanuel <emmanuelokello294@gmail.com>
2024-03-16 17:13:47 +0300

    completes the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+
+if (mainElement) {
+  mainElement.parentNode.removeChild(mainElement); 
+}
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Okelo is the champion"; 
+
